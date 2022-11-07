73a52f0c660c964e49a468342e26686bb4239b20
Francisco PÉREZ <francisco.perez@solera.com>
2022-11-07 09:50:42 +0100

    add files

---
+++ src/main/java/com/example/questionnaire/blockchainquestionnaire/BlockChainQuestionnaireApplication.java
@@ -0,0 +1,13 @@
+package com.example.questionnaire.blockchainquestionnaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockChainQuestionnaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockChainQuestionnaireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/questionnaire/blockchainquestionnaire/BlockChainQuestionnaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.questionnaire.blockchainquestionnaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockChainQuestionnaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
